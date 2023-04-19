372a3ecff56a64d836ca4aab6ac47d4549a70f57
Shar068 <108710778+Shar068@users.noreply.github.com>
2023-04-19 16:23:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
 st.write("Hello World")
+
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
